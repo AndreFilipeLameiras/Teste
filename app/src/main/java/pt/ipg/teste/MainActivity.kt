bffd590faa3cb313a93255b5488a7ba85e2da7a9
1700017 <afplameiras@gmail.com>
2022-04-27 10:38:56 +0100

    criaçao de funçao enviarDados

--- app/src/main/java/pt/ipg/teste/MainActivity.kt
+++ app/src/main/java/pt/ipg/teste/MainActivity.kt
@@ -10,8 +10,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val buttonEnviar = findViewById<Button>(R.id.buttonEnviar)
+        buttonEnviar.setOnClickListener { enviarDados() }
 
     }
 
+    private fun enviarDados(){
+        val editTextNome = findViewById<EditText>(R.id.editTextNome)
+        val nome = editTextNome.text.toString()
+
+        if(nome.isBlank()){
+            editTextNome.error = getString(R.string.Nome_Obrigatorio)
+            editTextNome.requestFocus()
+            return
+        }
+
+        val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
+        val email = editTextEmail.text.toString()
+
+        if(nome.isBlank()){
+            editTextEmail.error = getString(R.string.Email_obrigatorio)
+            editTextEmail.requestFocus()
+            return
+        }
+
+        val editTextTelefone = findViewById<EditText>(R.id.editTextTelefone)
+        
+    }
 
 }
